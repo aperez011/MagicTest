e8dd143faa49667d2a82e79746f8cb67d6c1cb97
RDMBG\superuser <apseverino011@gmail.com>
2017-12-19 12:25:29 -0400

    First Part

---
+++ MT.Services/ClinicaServices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MT.Entities;
+
+namespace MT.Services
+{
+    class ClinicaServices : General.IBasicMethods<Clinica>
+    {
+        public List<Clinica> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Clinica GetBy(Guid gid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save(Clinica entity)
+        {
+            return this.Save(new List<Clinica>() { entity });
+        }
+
+        public bool Save(List<Clinica> entities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MT.Services/General/IBasicMethods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MT.Services.General
+{
+    public interface IBasicMethods<T> where T : class
+    {
+
+        List<T> GetAll();
+
+        T GetBy(Guid gid);
+
+        bool Save(List<T> entities);
+
+    }
+}
